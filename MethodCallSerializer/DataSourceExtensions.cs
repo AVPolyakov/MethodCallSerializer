bfb27643164ef11156566d925f9732d90b2d3e2d
AVPolyakov <alexandr.polyakov@gmail.com>
2016-11-19 03:25:18 +0300

    Add async

--- MethodCallSerializer/DataSourceExtensions.cs
+++ MethodCallSerializer/DataSourceExtensions.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 using Newtonsoft.Json;
 
 namespace MethodCallSerializer
@@ -28,12 +29,27 @@ namespace MethodCallSerializer
             public T Call<T>(string methodName, Args args)
             {
                 var result = (T) typeof (TResult).GetMethod(methodName).Invoke(it, args.Value);
-                File.WriteAllText(GetCachePath<TResult>(methodName), JsonConvert.SerializeObject(result));
-                return result;
+                var type = typeof (T);
+                if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof (Task<>))
+                    return (T) typeof (DataSource).GetMethod(nameof(SerializeTaskResult))
+                        .MakeGenericMethod(type.GetGenericArguments())
+                        .Invoke(null, new object[] {result, GetCachePath<TResult>(methodName)});
+                else
+                {
+                    File.WriteAllText(GetCachePath<TResult>(methodName), JsonConvert.SerializeObject(result));
+                    return result;
+                }
             }
 
             public void CallVoid(string methodName, Args args) 
                 => typeof (TResult).GetMethod(methodName).Invoke(it, args.Value);
+        }
+
+        public static async Task<T> SerializeTaskResult<T>(object o, string path)
+        {
+            var result = await (Task<T>) o;
+            File.WriteAllText(path, JsonConvert.SerializeObject(result));
+            return result;
         }
 
         private static string GetCachePath<TResult>(string methodName)
@@ -52,13 +68,24 @@ namespace MethodCallSerializer
 
         public class ImplementationImpl<TResult> : IImplementation
         {
-            public T Call<T>(string methodName, Args args) 
-                => JsonConvert.DeserializeObject<T>(File.ReadAllText(GetCachePath<TResult>(methodName)));
+            public T Call<T>(string methodName, Args args)
+            {
+                var type = typeof (T);
+                if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof (Task<>))
+                    return (T) typeof (DataSource).GetMethod(nameof(DeserializeTaskResult))
+                        .MakeGenericMethod(type.GetGenericArguments())
+                        .Invoke(null, new object[] {GetCachePath<TResult>(methodName)});
+                else
+                    return JsonConvert.DeserializeObject<T>(File.ReadAllText(GetCachePath<TResult>(methodName)));
+            }
 
             public void CallVoid(string methodName, Args args)
             {
                 throw new Exception();
             }
         }
+
+        public static Task<T> DeserializeTaskResult<T>(string path) 
+            => Task.FromResult(JsonConvert.DeserializeObject<T>(File.ReadAllText(path)));
     }
 }
--- MethodCallSerializer/Program.cs
+++ MethodCallSerializer/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace MethodCallSerializer
 {
@@ -7,8 +8,16 @@ namespace MethodCallSerializer
     {
         static void Main()
         {
-            new DataSource1().AsDataSource(default(IDataSource1)).M1("Test1");
-            if (DataSource.Create<IDataSource1>().M1("Test2")[0] != "Test1")
+            new DataSource1().AsDataSource(default(IDataSource1)).M1("2");
+            if (DataSource.Create<IDataSource1>().M1("Test2")[0] != "2")
+                throw new Exception();
+            MainAsync().Wait();
+        }
+
+        private static async Task MainAsync()
+        {
+            await new DataSource1().AsDataSource(default(IDataSource1)).M2("1");
+            if ((await DataSource.Create<IDataSource1>().M2("Test2"))[0] != "1")
                 throw new Exception();
         }
     }
@@ -16,10 +25,12 @@ namespace MethodCallSerializer
     public class DataSource1 : IDataSource1
     {
         public List<string> M1(string x) => new List<string> {x};
+        public Task<List<string>> M2(string x) => Task.FromResult(new List<string> {x});
     }
 
     public interface IDataSource1
     {
         List<string> M1(string x);
+        Task<List<string>> M2(string x);
     }
 }
